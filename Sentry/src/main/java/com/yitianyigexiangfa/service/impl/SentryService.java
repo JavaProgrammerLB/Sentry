92d32b24345d1d2236c0ee8969802c3dd784aaf8
LiuBei <liubeitongxue@icloud.com>
2016-10-20 23:39:30 +0800

    update service

--- Sentry/src/main/java/com/yitianyigexiangfa/service/impl/SentryService.java
+++ Sentry/src/main/java/com/yitianyigexiangfa/service/impl/SentryService.java
@@ -1,8 +1,11 @@
 package com.yitianyigexiangfa.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.yitianyigexiangfa.dao.ProgrammeDao;
 import com.yitianyigexiangfa.model.Programme;
 import com.yitianyigexiangfa.spider.LjswSpider;
 
@@ -11,8 +14,29 @@ public class SentryService {
 	
 	@Autowired
 	private LjswSpider ljsw;
+	@Autowired
+	private ProgrammeDao programmeDao;
 	
+	
+	// spider here
 	public Programme getLjsw(){
 		return ljsw.getNewestLjsw();
 	}
+	
+	// email here
+	public void sendEmail(){
+		System.out.println("发送一封邮件");
+	}
+	
+	// timer task here
+	
+	// crud
+	public void addProgramme(Programme pro){
+		programmeDao.insertProgramme(pro);
+	}
+	
+	public List<Programme> getProgrammeByPrimaryKey(int vol){
+		return programmeDao.getProgrammeByPrimaryKey(vol);
+	}
+	
 }
