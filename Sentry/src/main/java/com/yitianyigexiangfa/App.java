acb5a6749c51aa2d19205888e3ddac3c0a80a3b7
LiuBei <liubeitongxue@icloud.com>
2016-11-30 14:02:20 +0800

    第一次加上定时器功能

--- Sentry/src/main/java/com/yitianyigexiangfa/App.java
+++ Sentry/src/main/java/com/yitianyigexiangfa/App.java
@@ -1,15 +1,25 @@
 package com.yitianyigexiangfa;
 
+import java.util.Date;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 import com.yitianyigexiangfa.config.WebConfig;
 import com.yitianyigexiangfa.service.impl.SentryService;
 
 @Configuration
 @ComponentScan({"com.yitianyigexiangfa"})
-public class App {
+@EnableScheduling
+public class App implements SchedulingConfigurer{
 
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(App.class);
@@ -17,4 +27,31 @@ public class App {
 		ctx.registerShutdownHook();
 	}
 
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+		// 
+		taskRegistrar.setTaskScheduler(taskScheduler());
+	}
+	
+	@Bean
+	public TaskScheduler taskScheduler(){
+		//
+		ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+		
+		taskScheduler.setDaemon(true);
+		
+		return taskScheduler;
+	}
+	
+	@Bean
+	public ScheduledBean scheduledBean(){
+		return new ScheduledBean();
+	}
+}
+
+class ScheduledBean{
+	@Scheduled(fixedRate=1000)
+	void hell(){
+		System.out.println("hello,liubei" + new Date());
+	}
 }
